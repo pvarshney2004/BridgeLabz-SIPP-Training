d15ecb01a075c2428941cc7b219a9e7578a79d1b
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-06-17 11:40:53 +0530

    Prashant add program to check first number is smallest or not

---
+++ ControlFlow/is_first_smallest.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class is_first_smallest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+        boolean flag=false;
+        if(num1<num2 && num1<num3){
+            flag=true;
+        }
+        System.out.println("Is the first number the smallest? "+flag);
+    }
+}
