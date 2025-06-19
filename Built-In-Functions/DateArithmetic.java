6a7f682fff540b731113bfa122140528296bcca1
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-06-19 12:17:16 +0530

    Prashant add program to add days,months,years in current date

---
+++ Built-In-Functions/DateArithmetic.java
@@ -0,0 +1,18 @@
+import java.time.*;
+import java.util.Scanner;
+
+public class DateArithmetic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // input from user
+        String inputDate = sc.nextLine();
+        // parse user input
+        LocalDate date = LocalDate.parse(inputDate);
+        // perform some operations on date
+        LocalDate modified = date.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
+        // final output
+        System.out.println(modified);
+        sc.close();
+
+    }
+}
