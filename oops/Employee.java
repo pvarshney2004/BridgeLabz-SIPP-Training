1b2755189f9f909872517201a95d8a3f1e3dfe09
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-06-24 08:32:17 +0530

    Prashant add program to implement employee class and print its details

---
+++ oops/Employee.java
@@ -0,0 +1,10 @@
+package oops;
+
+public class Employee {
+	int id;
+	String name;
+	int salary;
+	public void display() {
+		System.out.println(id + " " + name + " " + salary);
+	}
+}
---
+++ oops/EmployeeClient.java
@@ -0,0 +1,12 @@
+package oops;
+
+public class EmployeeClient {
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.id = 101;
+		emp.name = "prashant";
+		emp.salary = 10000;
+		emp.display();
+		
+	}
+}
