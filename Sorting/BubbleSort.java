63905d405dfb07b752b1b3542d31da033b17a831
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-07-10 09:55:56 +0530

    Prashant add program BubbleSort

---
+++ Sorting/BubbleSort.java
@@ -0,0 +1,22 @@
+package Sorting;
+
+public class BubbleSort {
+	public static void main(String[] args) {
+		int[] arr = {4,5,3,2,1};
+		BubbleSort(arr);
+		for(int i=0; i<arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+	}
+	public static void BubbleSort(int[] arr) {
+		for(int turn = 1; turn<arr.length; turn++) {
+			for(int i=0; i<arr.length-turn; i++) {
+				if(arr[i]>arr[i+1]) {
+					int temp = arr[i];
+					arr[i] = arr[i+1];
+					arr[i+1] = temp;
+				} 
+			}
+		}
+	}
+}
