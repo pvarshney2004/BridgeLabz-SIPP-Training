c2b8cb7868ccb7a1190a18d003f85302aa7f44cc
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-07-11 09:20:34 +0530

    Prashant add program to find first and last index of element

---
+++ Searching/First_and_Last.java
@@ -0,0 +1,41 @@
+package Searching;
+
+import java.util.Arrays;
+
+public class First_and_Last {
+	public static void main(String[] args) {
+		int[] nums = { 5, 7, 7, 8, 8, 10 };
+		int target = 8;
+		int[] result = { -1, -1 };
+		int left = binarySearch(nums, target, true);
+		int right = binarySearch(nums, target, false);
+		result[0] = left;
+		result[1] = right;
+		System.out.println(Arrays.toString(result));
+	}
+
+	public static int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
+		int left = 0;
+		int right = nums.length - 1;
+		int idx = -1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (nums[mid] > target) {
+				right = mid - 1;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				idx = mid;
+				if (isSearchingLeft) {
+					right = mid - 1;
+				} else {
+					left = mid + 1;
+				}
+			}
+		}
+
+		return idx;
+	}
+}
