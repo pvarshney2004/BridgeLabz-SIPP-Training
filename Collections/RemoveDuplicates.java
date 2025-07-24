86083834776077bbf38a9f659046d5970a3cedfc
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-07-24 11:22:15 +0530

    Prashant add program to remove duplicates

---
+++ Collections/RemoveDuplicates.java
@@ -0,0 +1,17 @@
+package Collections;
+
+import java.util.*;
+
+public class RemoveDuplicates {
+	public static void main(String[] args) {
+		List<Integer> ll = Arrays.asList(3, 1, 2, 2, 3, 4);
+		Set<Integer> set = new LinkedHashSet<Integer>();
+		for(int i : ll) {
+			set.add(i);
+		}
+//		System.out.println(set);
+		
+		
+		
+	}
+}
