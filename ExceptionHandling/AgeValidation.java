fe031a7e132c579b710e3e4be1d354733c400005
Prashant Varshney <prashantvarshney4321@gmail.com>
2025-08-12 09:31:53 +0530

    Prashant add program to implement custom exception

---
+++ ExceptionHandling/AgeValidation.java
@@ -0,0 +1,34 @@
+package ExceptionHandling;
+
+import java.util.Scanner;
+
+//Custom Exception
+class InvalidAgeException extends Exception {
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+}
+
+public class AgeValidation {
+
+	// Method to validate age
+	public static void validateAge(int age) throws InvalidAgeException {
+		if (age < 18) {
+			throw new InvalidAgeException("Age must be 18 or above");
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			System.out.print("Enter your age: ");
+			int age = sc.nextInt();
+			validateAge(age);
+			System.out.println("Access granted!");
+		} catch (InvalidAgeException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			sc.close();
+		}
+	}
+}
